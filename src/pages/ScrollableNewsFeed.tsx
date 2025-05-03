1dacd01fe1e654f1dc5093cae34d41912c29f0fe
Jyoti <jyoti.duwal@outcodesoftware.com>
2025-05-03 19:39:31 +0545

    Implements Swiper.

--- src/pages/ScrollableNewsFeed.tsx
+++ src/pages/ScrollableNewsFeed.tsx
@@ -1,12 +1,48 @@
+// ScrollableNewsFeed.tsx
 import Layout from "./Layout.tsx";
-import styles from './ScrollableNewsFeed.module.scss';
+import styles from "./ScrollableNewsFeed.module.scss";
+
+// Swiper core & required modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar, Mousewheel } from "swiper/modules";
+
+// Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+
+// Dummy data for now
+const dummyItems: string[] = [
+    "Breaking News: Market hits all-time high",
+    "Sports Update: City Team wins championship",
+    "Weather Alert: Heavy rains expected tomorrow",
+    "Tech: New flagship smartphone announced",
+    "Entertainment: Award show highlights and winners",
+    "Health: Tips for a balanced diet",
+    "Travel: Top 10 hidden gems to visit in 2025"
+];
 
 const ScrollableNewsFeed = () => {
     return (
         <Layout>
-            <div className={styles.container}>something</div>
+            <div className={styles.container}>
+                <Swiper
+                    direction="vertical"
+                    slidesPerView={1}
+                    spaceBetween={10}
+                    mousewheel={{ forceToAxis: true, releaseOnEdges: true }}
+                    scrollbar={{ draggable: true }}
+                    modules={[Mousewheel, Scrollbar]}
+                    className={styles.swiper}
+                >
+                    {dummyItems.map((item, idx) => (
+                        <SwiperSlide key={idx}>
+                            <div className={styles.slide}>{item}</div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </Layout>
-    )
-}
+    );
+};
 
 export default ScrollableNewsFeed;
